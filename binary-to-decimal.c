5a37deac8bc119407eb054e1a289c05000439e30
unknown <kearaabigaelburbos@gmail.com>
2025-08-21 15:03:01 +0000

    refined code

--- binary-to-decimal.c
+++ binary-to-decimal.c
@@ -4,21 +4,21 @@
 int main() {
 
     char bits[1000]; 
-    int binary = 1;
     int decimal = 0;
     int power = 1;
 
     printf("Input your binary: "); 
-    scanf("%s", bits); 
+    scanf("%s", &bits); 
 
     int binaryIndex = strlen(bits);
 
     for(int i= binaryIndex-1; i >= 0; i--) {
-        if(bits[i] == '1') {
-            decimal += binary * power;
-        } if(bits[i] == '0') {
-            decimal = decimal;
+        int binary = bits[i] - 48; 
+        if(binary < 0 || binary > 1) {
+            printf("This is not a valid binary number.");
+            return 0;
         }
+        decimal += binary * power;
         power *= 2; 
     }
 
